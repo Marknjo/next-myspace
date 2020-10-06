c626181e8c57d47310874b5a4ea81c5551f12509
Mark Njoroge <marknjo7@gmail.com>
2020-10-06 09:45:58 +0300

    implement fetching of single post

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -10,7 +10,22 @@ interface IProps {
   params: { slug: string };
 }
 
-async function getPost(slug: string): IBlog | null {}
+async function getPost(slug: string): Promise<IBlog | null> {
+  const res = await fetch('http://localhost:300/content', {
+    next: {
+      revalidate: 420,
+    },
+  });
+  const data: IBlog[] = await res.json();
+
+  const foundPost = data.find((post) => post.slug === slug);
+
+  if (!foundPost) {
+    return null;
+  }
+
+  return foundPost;
+}
 
 export default async function PostPage({ params }: IProps) {
   const slug = params.slug;
