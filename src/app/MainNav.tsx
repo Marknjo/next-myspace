0084b6e1f3885a5c2d36dde87b2ff112ad07a4a1
Mark Njoroge <marknjo7@gmail.com>
2020-10-06 09:18:35 +0300

    add MainNav component

---
+++ src/app/MainNav.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './MainNav.module.css';
+
+export default function MainNav() {
+  return (
+    <nav className={styles.nav}>
+      <Link href='/'>
+        <Image src='/logo.svg' width={216} height={30} alt='NextSpace Logo' />
+      </Link>
+
+      <ul className={styles.links}>
+        <li>
+          <Link href='/about'>About</Link>
+        </li>
+        <li>
+          <Link href='/blog'>Blog</Link>
+        </li>
+        <li>
+          <Link href='/users'>Users</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
