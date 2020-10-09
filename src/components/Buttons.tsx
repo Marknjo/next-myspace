843cd64b54c3164fb39695084af830379c9405b1
Mark Njoroge <marknjo7@gmail.com>
2020-10-09 11:06:50 +0300

    complete implementing SignInButton component

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,9 +1,30 @@
 'use client';
 
-import { signOut } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export function SignInButton() {
-  return <button>Sign In</button>;
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <>...</>;
+  }
+
+  if (status === 'authenticated') {
+    return (
+      <Link href='/dashboard'>
+        <Image
+          src={session.user?.image ?? '/mememan.webp'}
+          width={32}
+          height={32}
+          alt={session.user?.name ?? 'Profile image'}
+        />
+      </Link>
+    );
+  }
+
+  return <button onClick={() => signIn()}>Sign In</button>;
 }
 
 export function SignOutButton() {
