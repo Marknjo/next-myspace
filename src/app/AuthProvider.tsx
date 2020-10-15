13f2e8c9b10ce4e1a178061992d4781a320e58a3
Mark Njoroge <marknjo7@gmail.com>
2020-10-15 12:43:37 +0300

    refactor AuthProvider to take in session prop

--- src/app/AuthProvider.tsx
+++ src/app/AuthProvider.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
+import { SessionProvider, useSession } from 'next-auth/react';
 import { ReactNode } from 'react';
 
 interface IProps {
   children: ReactNode;
+  session: Session | null | undefined;
 }
 
-export default function AuthProvider({ children }: IProps) {
-  return <SessionProvider>{children}</SessionProvider>;
+export default function AuthProvider({ session, children }: IProps) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
