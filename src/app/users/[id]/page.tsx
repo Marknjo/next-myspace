2c3c2bb37a34d22ed0ccc94abdd11c2832d82354
Mark Njoroge <marknjo7@gmail.com>
2020-10-17 14:31:10 +0300

    load user data from teh database

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,4 +1,17 @@
+import { db } from '@/src/server/db.server';
+import { User } from '@prisma/client';
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+async function getUser(id: string): Promise<User | null> {
+  const user = await db.user.findFirst({ where: { id } });
+
+  if (!user) {
+    return null;
+  }
+
+  return user;
+}
 
 interface IProps {
   params: {
@@ -6,19 +19,25 @@ interface IProps {
   };
 }
 
-export default function UserProfilePage({ params: { id } }: IProps) {
+export default async function UserProfilePage({ params: { id } }: IProps) {
+  const user = await getUser(id);
+
+  if (!user) {
+    notFound();
+  }
+
   return (
     <section>
-      <h1>User Name</h1>
+      <h1>{user.name}</h1>
       <Image
-        src={'/mememan.webp'}
+        src={user.image || '/mememan.webp'}
         width={300}
         height={300}
         alt={`Mememan's profile`}
       />
 
       <h3>Bio</h3>
-      <p>User bio</p>
+      <p>{user.bio || 'user does not have a bio text'}</p>
     </section>
   );
 }
