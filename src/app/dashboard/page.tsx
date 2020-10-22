a3df423d1eeaf2fa01b881fe2c68fde0c4ccfea3
Mark Njoroge <marknjo7@gmail.com>
2020-10-22 16:43:14 +0300

    refactor ProfileForm by putting server side content to page component /dashaboard/page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation';
 import { User } from '@prisma/client';
 import { db } from '@/src/server/db.server';
 import ProfileForm from './ProfileForm';
+import { revalidatePath } from 'next/cache';
 
 async function getUser(email: string): Promise<User> {
   const user = await db.user.findFirst({ where: { email: email } });
@@ -26,10 +27,40 @@ export default async function DashboardPage() {
 
   const user = await getUser(email);
 
+  async function handleAction(formData: FormData) {
+    'use server';
+    const formD = Object.fromEntries(formData) as unknown as User;
+
+    // add validations
+    formD.age = Number(formD.age);
+
+    const data = {
+      name: formD.name,
+      image: formD.image,
+      age: formD.age,
+      bio: formD.bio,
+    };
+
+    // submit data
+    try {
+      const updatedUser = await db.user.update({
+        where: {
+          id: user.id,
+        },
+        data,
+      });
+      return updatedUser;
+    } catch (error) {
+      throw error;
+    }
+
+    revalidatePath('/dashboard');
+  }
+
   return (
     <>
       <h1>Dashboard</h1>
-      <ProfileForm user={user} />
+      <ProfileForm user={user} onSubmit={handleAction} />
     </>
   );
 }
