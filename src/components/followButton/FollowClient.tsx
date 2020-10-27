508eb9a31b9be4ac99401751b196e3a102c5d1e4
Mark Njoroge <marknjo7@gmail.com>
2020-10-27 18:21:11 +0300

    implement FollowButton client unfollowHandler

--- src/components/followButton/FollowClient.tsx
+++ src/components/followButton/FollowClient.tsx
@@ -1,10 +1,41 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { useState, useTransition } from 'react';
+
 interface IProps {
   targetUserId: string;
   isFollowing: boolean;
 }
 
 export default function FollowClient({ targetUserId, isFollowing }: IProps) {
-  return null;
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isMutating = isFetching || isPending;
+
+  async function unfollowHandler() {
+    setIsFetching(true);
+
+    const res = await fetch(`/api/follow?targetUserId=${targetUserId}`, {
+      method: 'DELETE',
+    });
+
+    setIsFetching(false);
+    startTransition(() => router.refresh());
+  }
+
+  async function followHandler() {}
+
+  if (isFollowing) {
+    return (
+      <button onClick={unfollowHandler}>
+        {!isMutating ? 'Unfollow' : '...'}
+      </button>
+    );
+  }
+
+  return (
+    <button onClick={followHandler}>{!isMutating ? 'Follow' : '...'}</button>
+  );
 }
