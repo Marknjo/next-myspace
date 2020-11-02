9344e085540a1bad921aeb5492c7604284f4fa56
Mark Njoroge <marknjo7@gmail.com>
2020-11-02 08:30:39 +0300

    implement a skeleton loader card in the /users/[id]

--- src/app/users/[id]/loading.tsx
+++ src/app/users/[id]/loading.tsx
@@ -1,7 +1,15 @@
+import styles from './page.module.scss';
+
 export default function LoadingUser() {
   return (
-    <section>
-      <p>Loading user data...</p>
-    </section>
+    <article className={styles.card}>
+      <h1 className={`${styles.cardTitle} ${styles.skeleton}`}></h1>
+      <div className={`${styles.cardImg} ${styles.skeleton}`}></div>
+      <div className={styles.cardBody}>
+        <h2 className={`${styles.cardSubTitle} ${styles.skeleton}`}></h2>
+        <p className={`${styles.cardIntro} ${styles.skeleton}`}></p>
+        <button className={`${styles.btn} ${styles.skeleton}`}></button>
+      </div>
+    </article>
   );
 }
